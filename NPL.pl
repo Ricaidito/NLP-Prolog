f990acc040b8e8bbe8b7e5e762cf07f0f1736152
Carlos Collado <carlose0808@hotmail.com>
2022-07-23 10:01:39 -0400

    Manage to work in first case

--- NPL.pl
+++ NPL.pl
@@ -1,30 +1,34 @@
 % op(500,xfy,->).
 % op(500,xfy,&).
 
-sentence(P) --> noun_phrase(X,P1,P), verb_phrase(X,P1).
+sentence(P) --> noun_phrase(X,P1,N,P), verb_phrase(X,N,P1).
 
-noun_phrase(X,P1,P) --> determiner(X,P2,P1,N,P), noun(X,N,P3), rel_clause(X,P3,P2).
+noun_phrase(X,P1,N,P) --> determiner(X,P2,P1,N,P), noun(X,N,P3), rel_clause(X,P3,N,P2).
 
-noun_phrase(X,P,P) --> proper_noun(X).
+% verb_phrase(X,N,P) --> trans_verb(X,Y,P1), noun_phrase(Y,P1,N,P).
 
-verb_phrase(X,P) --> trans_verb(X,Y,P1), noun_phrase(Y,P1,P).
+verb_phrase(X,N,P) --> intrans_verb(X,N,P).
 
-verb_phrase(X,P) --> intrans_verb(X,P).
+rel_clause(X, P1, N, (P1&P2)) --> [that], verb_phrase(X,N,P2).
 
-rel_clause(X, P1, (P1&P2)) --> [that], verb_phrase(X,P2).
+rel_clause(_,P,_,P) --> [].
 
-rel_clause(_,P,P) --> [].
+determiner(X, P1, P2, plural, all(X,(P1 -> P2))) --> ['All'].
 
-determiner(X, P1, P2, plural, all(X,(P1 -> P2))) --> [every].
+determiner(X, P1, P2, plural, all(X,(P1 -> P2))) --> [all].
 
-determiner(X, P1, P2, singular, exists(X, (P1&P2))) --> [a].
+determiner(X, P1, P2, singular, exists(X, (P1 & P2))) --> ['Some'].
 
-noun(X, man(X)) --> [man].
+determiner(X, P1, P2, singular, exists(X, (P1 & P2))) --> [some].
 
-noun(X, woman(X)) --> [woman].
+noun(X, singular, boy(X)) --> [boy].
 
-proper_noun(john) --> [john].
+noun(X, plural, boys(X)) --> [boys].
 
-trans_verb(X, Y, loves(X,Y)) --> [loves].
+% noun(X, woman(X)) --> [woman].
 
-intrans_verb(X, lives(X)) --> [lives].
+% trans_verb(X, Y, loves(X,Y)) --> [loves].
+
+intrans_verb(X,plural,run(X)) --> [run].
+
+intrans_verb(X,singular,runs(X)) --> [runs].
